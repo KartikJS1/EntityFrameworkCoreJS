14d9853625b408bbaa7e5fb4dac092060f117d35
KartikJS1 <kartikrameshchavan2003@gmail.com>
2026-06-09 19:43:02 +0530

    feat: adds all nuget packages for entity framework

--- EFCoreJS/obj/Debug/net9.0/EFCoreJS.AssemblyInfo.cs
+++ EFCoreJS/obj/Debug/net9.0/EFCoreJS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EFCoreJS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac091579385493d09f7e5484fab45d3455bdc12b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+086b473997eabcc5f92ea8d20c020f43b7856aa5")]
 [assembly: System.Reflection.AssemblyProductAttribute("EFCoreJS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EFCoreJS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
